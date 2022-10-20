c2dd4aff24e39d5932235eb20510bdbcc15db711
elnovere <eteresia58@students.calvin.ac.id>
2022-10-20 16:48:51 +0700

    membuat loginAdmin, loginCustomer, SignUp, Logout

--- ethree.py
+++ ethree.py
@@ -15,13 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
-
-
-
-
-
-
 # create database models
 class Customer(db.Model):
     __tablename__ = "customer"
@@ -60,16 +53,20 @@ class Employee(db.Model):
     __tablename__ = "employee"
     employee_id = db.Column(db.String(200), primary_key=True)
     name = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), unique=True, nullable=False)
     role_id = db.Column(db.String(200), db.ForeignKey('roletype.role_id'), nullable=False)
+    
 
     mActivity_re = db.relationship("ManagerActivity")
     pInvoice_re = db.relationship("PaymentInvoice", backref="payment_invoice")
     sInvoice_re = db.relationship("SaleInvoice")
 
-    def __init__(self, employee_id, name, role_id):
+    def __init__(self, employee_id, name, password,role_id):
         self.employee_id = employee_id
         self.name = name
+        self.password = password
         self.role_id = role_id
+        
 
 
 class ManagerActivity(db.Model):
@@ -153,8 +150,7 @@ class RoleType(db.Model):
 # customer page stuffs
 @app.route("/cust")
 def custPage():
-    session["curcust"] = "c-20221018180102"
-    curCust = session["curcust"]
+    curCust = session["customer_id"]
     return render_template("custPage.html", custTransactions=SaleInvoice.query.filter_by(customer_id=curCust).order_by(SaleInvoice.sale_date.asc()).all())
 
 @app.route("/addtransaction")
@@ -164,7 +160,7 @@ def newTransaction():
 @app.route("/addtransactionprocess", methods=["POST"])
 def newtransactionProcess():
     total = int(request.form.get("nominal"))
-    curCust = session["curcust"]
+    curCust = session["customer_id"]
     duration = int(request.form.get("durR"))
     remark_id = "WAITING"
 
@@ -187,10 +183,75 @@ def newtransactionProcess():
     return redirect(url_for('custPage'))
 
 
+# @app.route('/')
+# def dummy() :
+#     return render_template("dummy.html")
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    if request.method == 'POST':
+        cust = request.form['name']
+
+        found_cust = Customer.query.filter_by(name=cust).first()
+        if found_cust:
+            if request.form['password'] == found_cust.password:
+                session["customer_id"] = found_cust.customer_id
+                return redirect(url_for("custPage"))
+        
+        return redirect(url_for("signup"))
+    else:
+        return render_template('login.html')
+      
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():   
+    # cust = 
+    # # session["cust_name"] = cust
+    # found_cust = Customer.query.filter_by(name=request.form['name']).first() 
+    if request.method == 'POST':
+        cust = request.form['name']
+        found_cust = Customer.query.filter_by(name=cust).first() 
+        if found_cust:
+            return render_template('signup.html')
+        else:
+            cust_name = request.form['name']
+            address = request.form['address']
+            tel_num = request.form['tel_num']
+            password = request.form['password']
+            new_cust = Customer(cust_name,  address, tel_num, password)
+            db.session.add(new_cust)
+            db.session.commit()
+            return redirect(url_for("login"))
+    else:
+        return render_template('signup.html')
+      
+
+@app.route('/loginadmin', methods=['GET', 'POST'])
+def loginadmin(): 
+    if request.method == 'POST':
+        adminName = request.form['name']
+        session["adminName"] = adminName
+        # return redirect(url_for('cust'))
+
+        found_admin = Employee.query.filter_by(name=adminName).first()
+        if found_admin:
+            session['password'] = found_admin.password
+            if found_admin.role_id == 'a1':
+                return redirect(url_for("dummy")) #admin sale page
+            elif found_admin.role_id == 'a2':
+                return redirect(url_for("dummy")) #admin finance page
+            else:
+                return redirect(url_for("dummy")) #admin manager page
+        else:
+           return redirect(url_for("loginadmin"))
+    else:
+        return render_template('loginadmin.html')
 
 
-
-
+@app.route('/logout')
+def logout(): 
+    session.pop("customer_id", None)
+    return redirect(url_for("login"))
 
 
 
